8a01613bffea75df8dbec4211a036c39702cdfe8
stesiam <52320488+stesiam@users.noreply.github.com>
2022-09-26 22:33:10 +0300

    Add sidebar with sampling methods

--- ui.R
+++ ui.R
@@ -1,5 +1,30 @@
 ui <- fluidPage(
   
-  titlePanel(title = "Possible Samples Calculator", windowTitle = "Possible Samples")
+  includeCSS("www/custom.css"),
   
-)
+  tags$div(
+    titlePanel(title = "Possible Samples Calculator", windowTitle = "Possible Samples"),
+    class = "title"
+  ),
+  
+  sidebarLayout(
+    sidebarPanel(
+      br(),
+      selectInput(inputId = "method_sampling",
+                  label = "Choose your sampling method", 
+                  choices = 
+                    c("SRSWOR" = "srswor",
+                      "SRSWR" = "srswr",
+                      "Systematic" = "systematic",
+                      "Stratified" = "stratified"
+                    ),
+                  selected = "srswor"
+      )  
+    ),
+    
+    mainPanel(
+      tags$h3("Possible Samples")
+    )
+  )
+  
+) # end fluidPage
