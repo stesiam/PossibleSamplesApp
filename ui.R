2b462d54808bcc698fcb9043afe11263f43a2ec2
stesiam <52320488+stesiam@users.noreply.github.com>
2022-09-26 23:14:11 +0300

    Add conditional panels for every sampling method

--- ui.R
+++ ui.R
@@ -9,7 +9,7 @@ ui <- fluidPage(
   
   sidebarLayout(
     tags$div(
-    sidebarPanel(
+    sidebarPanel(width = 4,
       br(),
       selectInput(inputId = "method_sampling",
                   label = "Choose your sampling method", 
@@ -25,6 +25,14 @@ ui <- fluidPage(
         condition = "input.method_sampling == 'srswor' || input.method_sampling == 'srswr' || input.method_sampling == 'systematic'",
         numericInput(inputId = "population", label = "Population size", value = 20),
         numericInput(inputId = "sample", label = "Sample size", value = 5)
+      ),
+      conditionalPanel(
+        condition = "input.method_sampling == 'stratified'",
+        sliderInput(inputId = "strata", label = "Number of Strata", min = 1, max = 4, step = 1, value = 1),
+        fluidRow(column(6,
+                        numericInput(inputId = "N1", label = "N_1", value = 20)),
+                 column(6,
+                        numericInput(inputId = "n1", label = "n_1", value = 20)))
       ),
     ),
     id = "sidebar"),
