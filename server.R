43e4afa860ba1d063a748176522ed981d698eb3a
stesiam <52320488+stesiam@users.noreply.github.com>
2022-09-26 23:43:56 +0300

    Add server values using functions

--- server.R
+++ server.R
@@ -1,2 +1,6 @@
 server <- function(input, output) {
+  
+  output$num_srswor <- renderText({ possible_samples_srswor(input$population, input$sample) })
+  output$num_srswr <- renderText({possible_samples_srswr(input$population, input$sample) })
+  output$num_sys <- renderText({ possible_samples_systematic(input$population, input$sample) })
 }
